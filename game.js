861b37a81c3ef630727754ff5033da9f75501ff4
Siddhant Lodha <siddhant.lodha6@gmail.com>
2021-06-11 19:44:30 -0700

    fixed sarthak's weird stuff

--- game.js
+++ game.js
@@ -27,7 +27,7 @@ var game = new Phaser.Game(config);
 function preload() {
     //no images yet. need to get actual imgs
     this.load.image("background", "background.png")
-    this.load.image("spaceman", "spaceman.png")
+    this.load.image("spaceman", "spacemanright.png")
     this.load.image("asteroid1", "//asteroids/Stones_15.png")
     this.load.image("asteroid2", "//asteroids/Stones2Filled_02.png")
     this.load.image("asteroid3", "//asteroids/Stones2Filled_17.png")
@@ -37,15 +37,13 @@ function preload() {
 function create() {
     this.add.image(1920 / 2, 1080 / 2, "background") //load background centered
     player = this.physics.add.sprite(1800, 1080 / 2, "spaceman") //load astronot
-    asteroids = this.physics.add.staticGroup()
-    asteroids.create(x, y, 'asteroid1')
-    asteroids.create(x, y, 'asteroid2')
-    asteroids.create(x, y, 'asteroid3')
-    asteroids.create(x, y, 'asteroid2')
-    asteroids.create(x, y, 'asteroid3')
-    player = this.physics.add.sprite(1800, 1080 / 2, "spaceman") //load astronot
     player.setScale(0.3)
-    player.setCollideWorldBounds(true);
+    asteroids = this.physics.add.staticGroup()
+    //    asteroids.create(x, y, 'asteroid1')
+    //    asteroids.create(x, y, 'asteroid2')
+    //    asteroids.create(x, y, 'asteroid3')
+    //    asteroids.create(x, y, 'asteroid2')
+    //    asteroids.create(x, y, 'asteroid3')
 }
 
 function update() {
